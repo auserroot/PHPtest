87f6b424580bee47116d51e07cca0a94bcededcf
auserroot <fauserfzy@gmail.com>
2020-12-14 15:34:20 +0800

    main

--- tempCodeRunnerFile.js
+++
@@ -1,7 +0,0 @@
-var http = require('http');
-
-http.get({'host': 'myip.ipip.net', 'port': 80, 'path': '/'}, function(resp) {
-  resp.on('data', function(ip) {
-    console.log("My public IP address is: " + ip);
-  });
-});
